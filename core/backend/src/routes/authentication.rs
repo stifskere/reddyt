7d65dff077631ad50f62bde6adf4f2a31165eb86
stifskere <esteve@memw.es>
2025-10-20 18:28:35 +0200

    feat(authentication): Allow Cookie Authentication.

--- core/backend/src/routes/authentication.rs
+++ core/backend/src/routes/authentication.rs
@@ -1,9 +1,11 @@
 use actix_failwrap::{proof_route, ErrorResponse};
+use actix_web::cookie::{Cookie};
+use actix_web::cookie::time::Duration;
 use actix_web::{HttpResponse, Scope};
 use actix_web::web::scope;
 use thiserror::Error;
 
-use crate::utils::extractors::authentication::OptionalAuth;
+use crate::utils::extractors::authentication::{OptionalAuth, COOKIE_KEY};
 
 #[derive(ErrorResponse, Error, Debug)]
 enum AuthenticationRequestError {
@@ -17,18 +19,26 @@ pub fn authentication_scope() -> Scope {
         .service(login_route)
 }
 
-
 #[proof_route("POST /login")]
 async fn login_route(auth: OptionalAuth) -> Result<HttpResponse, AuthenticationRequestError> {
-    if !auth.is_authenticated() {
-        return Err(AuthenticationRequestError::Unauthorized);
+    match auth.token() {
+        Some(token) => {
+            let cookie = Cookie::build(COOKIE_KEY, token)
+                .path("/")
+                .http_only(true)
+                .secure(cfg!(not(debug_assertions)))
+                .max_age(Duration::hours(3))
+                .finish();
+
+            Ok(
+                HttpResponse::NoContent()
+                    .cookie(cookie)
+                    .finish()
+            )
+        }
+
+        None => Err(AuthenticationRequestError::Unauthorized)
     }
-
-
-    Ok(
-        HttpResponse::Ok()
-            .finish()
-    )
 }
 
 
