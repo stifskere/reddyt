117e91531dc97e2b34976ceb649988e10bb39881
stifskere <esteve@memw.es>
2025-10-22 04:08:09 +0200

    docs(configuration): Docuemnted the Application Config and Validated it.

--- core/backend/src/utils/application/configuration.rs
+++ core/backend/src/utils/application/configuration.rs
@@ -1,5 +1,22 @@
-use envconfig::Envconfig;
+use email_address::EmailAddress;
+use envconfig::{Envconfig, Error as EnvconfigError};
+use thiserror::Error;
 
+/// Holds any errors related to the configuration
+/// and application environment.
+#[derive(Error, Debug)]
+pub enum ReddytConfigError {
+    #[error("Couldn't load configuration from the environment, {0:#}")]
+    Envconfig(#[from] EnvconfigError),
+
+    #[error("The admin email at RYT_ADMIN_EMAIL is not valid.")]
+    InvalidEmail
+}
+
+/// The application relevant environment variables.
+///
+/// **This does not load `.env`, that must be done
+/// before loading this structure.**
 #[derive(Debug, Envconfig)]
 pub struct ReddytConfig {
     #[envconfig(from = "RYT_ADMIN_EMAIL")]
@@ -9,11 +26,32 @@ pub struct ReddytConfig {
 }
 
 impl ReddytConfig {
+    /// This function loads the environment with
+    /// a layer of validation, this is required for
+    /// self-hosters to debug any missconfiguration.
+    pub fn load_validated() -> Result<Self, ReddytConfigError> {
+        let initialized = Self::init_from_env()?;
+
+        // Since we use the admin email for basic authentication
+        // it must not contain colons, for future proofing
+        // the email can be validated as-is, this way alerts
+        // can be sent if needed.
+        if !EmailAddress::is_valid(initialized.admin_email()) {
+            return Err(ReddytConfigError::InvalidEmail);
+        }
+
+        Ok(initialized)
+    }
+
+    /// The application configured email
+    /// to access the admin panel.
     #[inline]
     pub fn admin_email(&self) -> &str {
         &self.admin_email
     }
 
+    /// The application configured password
+    /// to access the admin panel.
     #[inline]
     pub fn admin_password(&self) -> &str {
         &self.admin_password
--- core/backend/src/utils/application/context.rs
+++ core/backend/src/utils/application/context.rs
@@ -1,28 +1,38 @@
 use std::sync::Arc;
 
 use thiserror::Error;
-use envconfig::{Envconfig, Error as EnvconfigError};
 
-use crate::utils::application::configuration::ReddytConfig;
+use crate::utils::application::configuration::{ReddytConfig, ReddytConfigError};
 
+/// Holds any errors related to the application context
+/// i.e database connections, environment...
 #[derive(Error, Debug)]
 pub enum AppContextError {
     #[error("Error while loading coniguration, {0:#}")]
-    Config(#[from] EnvconfigError)
+    Config(#[from] ReddytConfigError)
 }
 
+/// The application context, registered as data in the
+/// HTTP service, can be accessed by anything else
+/// registered in it.
+///
+/// Holds contextual data such as database connections,
+/// environment, syncronization...
 #[derive(Clone, Debug)]
 pub struct AppContext {
     config: Arc<ReddytConfig>
 }
 
 impl AppContext {
+    /// Initialize the context with documented
+    /// defaults.
     pub fn new() -> Result<Self, AppContextError> {
         Ok(Self {
-            config: Arc::new(ReddytConfig::init_from_env()?)
+            config: Arc::new(ReddytConfig::load_validated()?)
         })
     }
 
+    /// The application environment configuration.
     #[inline]
     pub fn config(&self) -> &ReddytConfig {
         &self.config
