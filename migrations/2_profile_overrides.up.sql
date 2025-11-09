e203d9c65aff97c21a367e2d29fe0671361328c3
stifskere <esteve@memw.es>
2025-11-09 05:10:14 +0100

    feat(schemas): Add Profile Override Schema.

--- migrations/2_profile_overrides.up.sql
+++ migrations/2_profile_overrides.up.sql
@@ -1,1 +1,7 @@
--- Add up migration script here
+
+CREATE TABLE IF NOT EXISTS profile_overrides (
+	id SERIAL NOT NULL,
+	profile_id INTEGER REFERENCES profiles(id) NOT NULL, -- the profile this override is for
+	runs_at TIMESTAMP NOT NULL, -- when should the profile run at
+	claimed BOOLEAN NOT NULL DEFAULT false -- whether the profile is alraedy ran/claimed or not.
+);
